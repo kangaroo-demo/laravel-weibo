eac8f64f5df1f6deab8f786002715e3a90a6754a
Kangaroo <kangaroo-demo@qq.com>
2019-01-08 05:53:49 +0000

    用户注册页面

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,10 @@
 |
 */
 
+/* 静态页面 */
 Route::get('/', 'StaticPagesController@home')->name('home');
 Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
+
+/* 用户 */
+Route::get('signup', 'UsersController@create')->name('signup');
