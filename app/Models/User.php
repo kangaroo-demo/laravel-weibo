ce3b96fdb58f6276b97ed8675804109b2ddf886c
Kangaroo <kangaroo-demo@qq.com>
2019-01-09 03:49:18 +0000

    show user info

--- app/Models/User.php
+++ app/Models/User.php
@@ -27,4 +27,15 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    /**
+     * The method is used to generate gravatar.
+     *
+     * @return string
+     */
+    public function gravatar($size = '100')
+    {
+      $hash = md5(strtolower(trim($this->attributes['email'])));
+      return "http://www.gravatar.com/avatar/$hash?s=$size";
+    }
 }
