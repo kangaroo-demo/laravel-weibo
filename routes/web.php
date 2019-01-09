e8d7da59d5daef4f60264bc4bd80bfe06f547b53
Kangaroo <kangaroo-demo@qq.com>
2019-01-09 07:16:12 +0000

    create session

--- routes/web.php
+++ routes/web.php
@@ -30,3 +30,9 @@ Route::resource('users', 'UsersController');
 // Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit');
 // Route::patch('/users/{user}', 'UsersController@update')->name('users.update');
 // Route::delete('/users/{user}', 'UsersController@destroy')->name('users.destroy');
+
+
+/* 会话控制路由 */
+Route::get('login', 'SessionsController@create')->name('login');
+Route::post('login', 'SessionsController@store')->name('login');
+Route::delete('logout', 'SessionsController@destroy')->name('logout');
