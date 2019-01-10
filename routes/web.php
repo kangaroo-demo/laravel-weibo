6eb82ae31cd3c465641c95a4698d9ca540faa48c
Kangaroo <kangaroo-demo@qq.com>
2019-01-10 05:10:22 +0000

    reset pasword

--- routes/web.php
+++ routes/web.php
@@ -22,13 +22,6 @@ Route::get('signup', 'UsersController@create')->name('signup');
 
 /* 用户控制资源路由 */
 Route::resource('users', 'UsersController');
-// Route::get('/users', 'UsersController@index')->name('users.index');
-// Route::get('/users/create', 'UsersController@create')->name('users.create');
-// Route::get('/users/{user}', 'UsersController@show')->name('users.show');
-// Route::post('/users', 'UsersController@store')->name('users.store');
-// Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit');
-// Route::patch('/users/{user}', 'UsersController@update')->name('users.update');
-// Route::delete('/users/{user}', 'UsersController@destroy')->name('users.destroy');
 Route::get('signup/confirm/{token}', 'UsersController@confirmEmail')->name('confirm_email');
 
 
@@ -36,3 +29,9 @@ Route::get('signup/confirm/{token}', 'UsersController@confirmEmail')->name('conf
 Route::get('login', 'SessionsController@create')->name('login');
 Route::post('login', 'SessionsController@store')->name('login');
 Route::delete('logout', 'SessionsController@destroy')->name('logout');
+
+/* 用户重设密码 */
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
