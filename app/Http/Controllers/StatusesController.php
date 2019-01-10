04f3cdf2695ae6bd09fbc2711df3e5ae4df5eb7e
Kangaroo <kangaroo-demo@qq.com>
2019-01-10 09:33:01 +0000

    delete status

--- app/Http/Controllers/StatusesController.php
+++ app/Http/Controllers/StatusesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Auth;
+use App\Models\Status;
 
 class StatusesController extends Controller
 {
@@ -25,6 +26,14 @@ class StatusesController extends Controller
 
         session()->flash('success', '您已成功发布一条微博!');
 
+        return redirect()->back();
+    }
+
+    public function destroy(Status $status)
+    {
+        $this->authorize('destroy', $status);
+        $status->delete();
+        session()->flash('success', '微博已被成功删除！');
         return redirect()->back();
     }
 }
